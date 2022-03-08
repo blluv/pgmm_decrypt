a5e3f783a97cdae1119c166eea232cf5aba1412c
blluv <biluv@protonmail.com>
2022-03-08 16:50:17 +0900

    Update dec_too_short_key.py

--- dec_too_short_key.py
+++ dec_too_short_key.py
@@ -1,11 +1,8 @@
 import struct
 from typing import List
 
-
-
 def rotr32(x, n):
     return (x >> n) | ((x << (32 - n)) & 0xFFFFFFFF)
-
 
 def rotl32(x, n):
     return ((x << n) & 0xFFFFFFFF) | (x >> (32 - n))
@@ -15,23 +12,27 @@ def bytesToWords(data: bytes) -> List[int]:
 
     words = []
     for i in range(len(data)//4):
-        words.append(struct.pack("<I", data[i*4:i*4+4])[0])
+        words.append(struct.unpack("<L", data[i*4:i*4+4])[0])
     return words
 
 def wordsToBytes(words: List[int]) -> bytes:
     buf = bytearray()
     for word in words:
-        buf.extend(struct.unpack("<I", word))
+        buf.extend(struct.pack("<L", word))
     return bytes(buf)
 
 def decrypt(words: List[int]):
-    assert len(words) == 16
+    assert len(words) == 4 # 16bytes
 
     for _ in range(8):
         words[2] = rotl32(words[2], 1) & 0xffffffff
         words[3] = rotr32(words[3] & 0xffffffff, 1)
         words[0] = rotl32(words[0], 1) & 0xffffffff
-        words[1] = rotr32(words[1] & 0xffffffff, 1)        
+        words[1] = rotr32(words[1] & 0xffffffff, 1)
+
+    [words[0], words[2]] = [words[2], words[0]]
+    [words[1], words[3]] = [words[3], words[1]]
+
     return words
 
 def xor(a, b):
@@ -40,8 +41,10 @@ def xor(a, b):
         w[i] = a[i] ^ b[i]
     return bytes(w)
 
-iv = bytes.fromhex("A0 47 E9 3D 23 0A 4C 62 A7 44 B1 A4 EE 85 7F BA")
-data = bytes.fromhex('76 84 91 97 BA DC 85 7F 72 DF 0D 91 54 12 3E 7D') # info.json에 있던 key decode한 값
+# import json
+# from base64 import b64decode
 
-data_words = bytesToWords(data)
-print(wordsToBytes(decrypt(data_words)))
+# iv = bytes.fromhex("A0 47 E9 3D 23 0A 4C 62 A7 44 B1 A4 EE 85 7F BA")
+# info = json.loads(open("info.json", "rb").read().decode("utf8"))
+# data_words = bytesToWords(b64decode(info["key"]))
+# print(xor(iv, wordsToBytes(decrypt(data_words))))
